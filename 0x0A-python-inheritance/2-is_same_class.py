07ec8d525abbca9aecd1130aff2cb3efb58777b5
Rensytimsy <rensytimsy03@gmail.com>
2024-01-08 20:47:07 +0300

    checks if true or false

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Below is a function that checks if type is the same as data type"""
+
+
+def is_same_class(obj, a_class):
+    """conditional rendering"""
+    if type(obj) == a_class:
+        return True
+    return False
