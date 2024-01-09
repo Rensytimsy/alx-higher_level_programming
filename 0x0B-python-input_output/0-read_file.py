5deaff3e31ebb34ffb9a4b1b1aecd6849031468c
Rensytimsy <rensytimsy03@gmail.com>
2024-01-09 22:07:07 +0300

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Below are instructions to open a file"""
 
+
 def read_file(filename=""):
     """openning a file using with"""
     with open(filename, mode="r", encoding="utf-8") as new_file:
