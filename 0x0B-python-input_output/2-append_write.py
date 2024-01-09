6699406bd1237bced8f06bdc5a47c41bcfdd39eb
Rensytimsy <rensytimsy03@gmail.com>
2024-01-09 22:43:37 +0300

    append text to a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/bin/usr/python3
+"""The function below append a string to a file"""
+
+
+def append_write(filename="", text=""):
+    """The code below appends the strinr to a file"""
+    with open(filename, "a", encoding="utf-8") as new_file:
+        return new_file.write(text)
