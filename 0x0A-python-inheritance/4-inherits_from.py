3911a7649310ad8e895b7c6f85350d798923c75f
Rensytimsy <rensytimsy03@gmail.com>
2024-01-09 12:19:34 +0300

    some pycodestyle changes

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """The function checks if the obj is a subclass and if it can return a bool"""
+
+
 def inherits_from(obj, a_class):
     """Below is the conditional rendering"""
     if issubclass(type(obj), a_class) and type(obj) != a_class:
