cbd1a38bf4031de3bb4840e78578711bfb96e0d0
Rensytimsy <rensytimsy03@gmail.com>
2024-01-09 12:31:52 +0300

    checked some pycodestyle

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -4,6 +4,9 @@
 
 class BaseGeometry:
     """Represent base geometry."""
+    def __init__(self):
+        pass
+
     def area(self):
         """Not implememnted method"""
         raise Exception("area not implemented")
