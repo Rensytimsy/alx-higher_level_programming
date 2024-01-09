fd4205e0246d408572920a454842003f2d0f133f
Rensytimsy <rensytimsy03@gmail.com>
2024-01-09 12:47:01 +0300

    geometry

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+class BaseGeometry():
+    def __init__(self):
+        pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """You can assume that name is a string"""
+        if type(value) != int:
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
