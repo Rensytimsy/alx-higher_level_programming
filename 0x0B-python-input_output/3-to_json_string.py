b37ec68fe967e0b321e852b3b7ef940210d1b11b
Rensytimsy <rensytimsy03@gmail.com>
2024-01-10 01:50:49 +0300

    json,dumps()

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""json file representation """
+import json
+
+
+def to_json_string(my_obj):
+    """The code below should return object represent"""
+    return json.dumps(my_obj)
