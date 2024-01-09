b753ddebb03b21eb4ae122683f7e1d6b8c23258d
Rensytimsy <rensytimsy03@gmail.com>
2024-01-09 22:44:55 +0300

    append text to a file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,5 +4,5 @@
 
 def append_write(filename="", text=""):
     """The code below appends the strinr to a file"""
-    with open(filename, "a", encoding="utf-8") as new_file:
+    with open(filename, mode="a", encoding="utf-8") as new_file:
         return new_file.write(text)
